59a152dda4ba8e8eb316a2e43be856cf93239369
amukasya <amukasya@nd.edu>
2018-03-20 21:47:36 -0400

    finally starting to show results

--- analyze.py
+++ analyze.py
@@ -3,7 +3,7 @@
 ##GLOBAL LISTS TO BE USED
 neg_keys = []
 pos_keys = []
-neg_emote_key = []
+neg_emote_keys = []
 pos_emote_keys = []
 chat1 = []
 chat2 = []
@@ -11,8 +11,8 @@ chat3 = []
 master_chat = []
 ##SETS UP DICTIONARY WITH TEAM NAMES AND MEMBERS FOR SENTIMENT ANALYSIS
 teams = dict()
-teams["boston_uprising"] = ["DreamKazper","Snow","Gamsu","NotE","Neko","Avast","STRIKER","Kalios","Mistakes","Kellex"]
-teams["dallas_fuel"] = ["HarryHook","aKm","Rascal","OGE","Taimou","Custa","Chipshajen","Mickie","Seagull","cocco","EFFECT"]
+teams["boston_uprising"] = ["dreamkazper","snow","gamsu","note","neko","avast","striker","kalios","mistakes","kellex"]
+teams["dallas_fuel"] = ["harryhook","akm","rascal","oge","taimou","custa","chipshajen","mickie","seagull","cocco","effect"]
 teams["la_valiant"] = ["Silkthread","Grimreality","Kariv","Fate","Verbo","Envy","Space","Numlocked","Unkoe","Agilities","Soon"]
 teams["ny_excelsior"] = ["Saebyeolbe","Meko","Pine","Janus","Jjonak","Mano","Libero","Ark"]
 teams["florida_mayhem"] = ["zappis","Manneten","Sayaplayer","Logix","Zebbosai","CWoosH","TviQ","aWesomeGuy","Zuppeh"]
@@ -20,35 +20,37 @@ teams["houston_outlaws"] = [ "Muma","Bani","Clockwork","Mendokusaii","Boink","FC
 teams["london_spitfire"] = ["Gesture","birdring","Bdosin","NUS","Hooreg","Fury","HaGoPeun","WooHyaL","Profit","TiZi","Closer"]
 teams["la_gladiators"] = ["Asher","Surefour","iRemiix","Fissure","Bischu","Shaz","Hydration","BigGoose"]
 teams["philly_fusion"] = ["Joemeister","Boombox","Carpe","Snillo","fragi","Eqo","ShaDowBurn","Neptuno","DayFly","Hotba","Poko","SADO"]
-teams["sf_shock"] = ["Moth","dhaK","super","BABYBAY","sinatraa","sleepy","Danteh","Nomy","iddqd","Nevix","Architect"]
+teams["sf_shock"] = ["moth","dhak","super","babybay","sinatraa","sleepy","danteh","nomy","iddqd","nevix","architect"]
 teams["seoul_dynasty"] = ["Bunny","Miro","XepheR","gido","Wekeed","Munchkin","ZUNBA","KuKi","tobi","ryujehong","Gambler","FLETA"]
 teams["shanghai_dragons"] = ["Geguri","Undead","MG","Roshan","Diya","Freefeel","Xushu","Fiveking","Altering","Ado","Sky","Fearless"]
 
 def load_keywords():
 	for line in open("negative.txt"):
-		neg_keys.append(line)
+		neg_keys.append(line.lower().rstrip())
 
 	for line in open("positive.txt"):
-		pos_keys.append(line)
+		pos_keys.append(line.lower().rstrip())
 
 	for line in open("negativeemotes.txt"):
-		neg_emote_keys.append(line)
+		neg_emote_keys.append(line.rstrip())
 
 	for line in open("positiveemotes.txt"):
-		pos_emote_keys.append(line)
+		pos_emote_keys.append(line.rstrip())
+	#print(neg_keys)
 
-def load_chats(file1, file2, file3):
+def load_chats(file1):#, file2, file3):
 	for line in open(file1):
 		chat1.append(line)
 
-	for line in open(file2):
-		chat2.append(line)
+	# for line in open(file2):
+	# 	chat2.append(line)
 
-	for line in open(file3):
-		chat3.append(line) 
+	# for line in open(file3):
+	# 	chat3.append(line)
+
 	master_chat.append(chat1)
-	master_chat.append(chat2)
-	master_chat.append(chat3)
+	# master_chat.append(chat2)
+	# master_chat.append(chat3)
 
 def analyze(team1, team2):
 	'''
@@ -58,38 +60,48 @@ def analyze(team1, team2):
 	if there is then we add or subtract a point toward that teams prediction
 	'''
 	chat_count = 1
+	team1_count = 0
+	team2_count = 0
 	for chats in master_chat:
 		for message in chats:
 			split = message.lower().split()
-			pos_key_exists = set(split).intersect(pos_keys)
-			neg_key_exists = set(split).intersect(neg_keys)
-			pos_emote_exists = set(split).intersect(pos_emote_keys)
-			neg_emote_exists = set(split).intersect(neg_emote_keys)
-			team1_player = set(split).intersect(teams[str(team1)])
-			team2_player = set(split).intersect(teams[str(team2)])
+			#print("split:", split)
+			pos_key_exists = set(split).intersection(set(pos_keys))
+			#print("pos_exist:", pos_key_exists)
+			neg_key_exists = set(split).intersection(set(neg_keys))
+			pos_emote_exists = set(split).intersection(set(pos_emote_keys))
+			neg_emote_exists = set(split).intersection(set(neg_emote_keys))
 
-			if (bool(pos_key_exists) or bool(pos_emote_keys)) is True and bool(team1_player) is True:
+			team1_player = set(split).intersection(set(teams[str(team1)]))
+			team2_player = set(split).intersection(set(teams[str(team2)]))
+			#print("pos_key_exists", bool(pos_key_exists))
+			#print("pos_emote_exists", bool(pos_emote_exists))
+			#print("pos_emote_exists", bool(team1_player))
+			#print(bool(team1_player))
+			if (bool(pos_key_exists) or bool(pos_emote_exists)) is True and bool(team1_player) is True:
 				team1_count += 1
 
-			if (bool(neg_key_exists) or bool(neg_emote_keys)) is True and bool(team1_player) is True:
+			if (bool(neg_key_exists) or bool(neg_emote_exists)) is True and bool(team1_player) is True:
 				team1_count -= 1
 
-			if (bool(pos_key_exists) or bool(pos_emote_keys)) is True and bool(team2_player) is True:
+			if (bool(pos_key_exists) or bool(pos_emote_exists)) is True and bool(team2_player) is True:
 				team2_count += 1
 
-			if (bool(neg_key_exists) or bool(neg_emote_keys)) is True and bool(team2_player) is True:
+			if (bool(neg_key_exists) or bool(neg_emote_exists)) is True and bool(team2_player) is True:
 				team2_count -= 1
-			print("For chat file %d: %s score %d, %s score %d " % (chat_count, team1, team1_count, team2, team2_count))
-			chat_count += 1
+		print("For chat file %d: %s score %d, %s score %d " % (chat_count, team1, team1_count, team2, team2_count))
+		chat_count += 1
 
 
 if __name__=="__main__":
 	file1 = input("Enter File 1\n")
-	file2 = input("Enter File 2\n")
-	file3 = input("Enter File 3\n")
+	# file2 = input("Enter File 2\n")
+	# file3 = input("Enter File 3\n")
 	team1 = input("Enter team 1\n")
 	team2 = input("Enter team 2\n")
-	load_chats(file1, file2, file3)
+	load_keywords()
+	load_chats(file1)#, file2, file3)
+	analyze(team1,team2)
 
 '''how i think shit will work:
 -people watching matches and recording the match chat in intervals
--- parse.py
+++ parse.py
@@ -3,10 +3,10 @@
 
 if __name__=="__main__":
 	new_list = []
-	for line in open("overwatchleague.log"):
+	for line in open("SanFran vs Dallas"):
 		data = line.split()
 		new_list.append(data[2:])
 
 	for line in new_list:
 		string = (" ").join(line)
-		open('new_test_log.txt', 'a').write("%s\n" % string)
+		open('san_dallas.txt', 'a').write("%s\n" % string)
