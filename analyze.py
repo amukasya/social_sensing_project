77957ce43fb61d44c40a653d78337dbda0ac6130
amukasya <amukasya@nd.edu>
2018-04-27 11:47:07 -0400

    added logs

--- analyze.py
+++ analyze.py
@@ -11,18 +11,18 @@ chat3 = []
 master_chat = []
 ##SETS UP DICTIONARY WITH TEAM NAMES AND MEMBERS FOR SENTIMENT ANALYSIS
 teams = dict()
-teams["boston_uprising"] = ["dreamkazper","snow","gamsu","note","neko","avast","striker","kalios","mistakes","kellex"]
-teams["dallas_fuel"] = ["harryhook","akm","rascal","oge","taimou","custa","chipshajen","mickie","seagull","cocco","effect"]
-teams["la_valiant"] = ["Silkthread","Grimreality","Kariv","Fate","Verbo","Envy","Space","Numlocked","Unkoe","Agilities","Soon"]
-teams["ny_excelsior"] = ["Saebyeolbe","Meko","Pine","Janus","Jjonak","Mano","Libero","Ark"]
-teams["florida_mayhem"] = ["zappis","Manneten","Sayaplayer","Logix","Zebbosai","CWoosH","TviQ","aWesomeGuy","Zuppeh"]
-teams["houston_outlaws"] = [ "Muma","Bani","Clockwork","Mendokusaii","Boink","FCTFCTN","LiNkzr","SPREE","Rawkus","Jake","Coolmatt"]
-teams["london_spitfire"] = ["Gesture","birdring","Bdosin","NUS","Hooreg","Fury","HaGoPeun","WooHyaL","Profit","TiZi","Closer"]
-teams["la_gladiators"] = ["Asher","Surefour","iRemiix","Fissure","Bischu","Shaz","Hydration","BigGoose"]
-teams["philly_fusion"] = ["Joemeister","Boombox","Carpe","Snillo","fragi","Eqo","ShaDowBurn","Neptuno","DayFly","Hotba","Poko","SADO"]
-teams["sf_shock"] = ["moth","dhak","super","babybay","sinatraa","sleepy","danteh","nomy","iddqd","nevix","architect"]
-teams["seoul_dynasty"] = ["Bunny","Miro","XepheR","gido","Wekeed","Munchkin","ZUNBA","KuKi","tobi","ryujehong","Gambler","FLETA"]
-teams["shanghai_dragons"] = ["Geguri","Undead","MG","Roshan","Diya","Freefeel","Xushu","Fiveking","Altering","Ado","Sky","Fearless"]
+teams["boston_uprising"] = ["dreamkazper","snow","gamsu","note","neko","avast","striker","kalios","mistakes","kellex", "boston", "uprising"]
+teams["dallas_fuel"] = ["harryhook","akm","rascal","oge","taimou","custa","chipshajen","mickie","seagull","cocco","effect", "dallas", "fuel"]
+teams["la_valiant"] = ["silkthread","grimreality","kariv","fate","verbo","envy","space","numlocked","unkoe","agilities","soon", "la", "valiant"]
+teams["ny_excelsior"] = ["saebyeolbe","meko","pine","janus","jjonak","mano","libero","ark", "new york", "ny", "excelsior"]
+teams["florida_mayhem"] = ["zappis","manneten","sayaplayer","logix","zebbosai","cwoosh","tviq","awesomeguy","zuppeh", "florida", "mayhem"]
+teams["houston_outlaws"] = [ "muma","bani","clockwork","mendokusaii","boink","fctfctn","linkzr","spree","rawkus","jake","coolmatt","houston", "outlaws"]
+teams["london_spitfire"] = ["gesture","birdring","bdosin","nus","hooreg","fury","hagopeun","woohyal","profit","tizi","closer","london","spitfile"]
+teams["la_gladiators"] = ["asher","surefour","iremiix","fissure","bischu","shaz","hydration","biggoose", "la", "gladiators"]
+teams["philly_fusion"] = ["joemeister","boombox","carpe","snillo","fragi","eqo","shadowburn","neptuno","dayfly","hotba","poko","sado","fusion", "philly"]
+teams["sf_shock"] = ["moth","dhak","super","babybay","sinatraa","sleepy","danteh","nomy","iddqd","nevix","architect","san fran", "shock"]
+teams["seoul_dynasty"] = ["bunny","miro","xepher","gido","wekeed","munchkin","zunba","kuki","tobi","ryujehong","gambler","fleta", "seoul", "dynasty"]
+teams["shanghai_dragons"] = ["geguri","undead","mg","roshan","diya","freefeel","xushu","fiveking","altering","ado","sky","fearless", "shanghai", "dragons"]
 
 def load_keywords():
 	for line in open("negative.txt"):
@@ -36,21 +36,21 @@ def load_keywords():
 
 	for line in open("positiveemotes.txt"):
 		pos_emote_keys.append(line.rstrip())
-	#print(neg_keys)
 
-def load_chats(file1):#, file2, file3):
+
+def load_chats(file1, file2, file3):
 	for line in open(file1):
 		chat1.append(line)
 
-	# for line in open(file2):
-	# 	chat2.append(line)
+	for line in open(file2):
+		chat2.append(line)
 
-	# for line in open(file3):
-	# 	chat3.append(line)
+	for line in open(file3):
+		chat3.append(line)
 
 	master_chat.append(chat1)
-	# master_chat.append(chat2)
-	# master_chat.append(chat3)
+	master_chat.append(chat2)
+	master_chat.append(chat3)
 
 def analyze(team1, team2):
 	'''
@@ -65,21 +65,19 @@ def analyze(team1, team2):
 	for chats in master_chat:
 		for message in chats:
 			split = message.lower().split()
-			#print("split:", split)
+
 			pos_key_exists = set(split).intersection(set(pos_keys))
-			#print("pos_exist:", pos_key_exists)
+
 			neg_key_exists = set(split).intersection(set(neg_keys))
 			pos_emote_exists = set(split).intersection(set(pos_emote_keys))
 			neg_emote_exists = set(split).intersection(set(neg_emote_keys))
 
 			team1_player = set(split).intersection(set(teams[str(team1)]))
+			#print(team1_player)
 			team2_player = set(split).intersection(set(teams[str(team2)]))
-			#print("pos_key_exists", bool(pos_key_exists))
-			#print("pos_emote_exists", bool(pos_emote_exists))
-			#print("pos_emote_exists", bool(team1_player))
-			#print(bool(team1_player))
+
 			if (bool(pos_key_exists) or bool(pos_emote_exists)) is True and bool(team1_player) is True:
-				team1_count += 1
+				team1_count += 1 
 
 			if (bool(neg_key_exists) or bool(neg_emote_exists)) is True and bool(team1_player) is True:
 				team1_count -= 1
@@ -95,12 +93,12 @@ def analyze(team1, team2):
 
 if __name__=="__main__":
 	file1 = input("Enter File 1\n")
-	# file2 = input("Enter File 2\n")
-	# file3 = input("Enter File 3\n")
+	file2 = input("Enter File 2\n")
+	file3 = input("Enter File 3\n")
 	team1 = input("Enter team 1\n")
 	team2 = input("Enter team 2\n")
 	load_keywords()
-	load_chats(file1)#, file2, file3)
+	load_chats(file1, file2, file3)
 	analyze(team1,team2)
 
 '''how i think shit will work:
